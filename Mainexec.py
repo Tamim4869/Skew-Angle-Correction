56a5ff18ff1617e9d8810625f94e8ef3487c6454
Tamim4869 <141699750+Tamim4869@users.noreply.github.com>
2023-11-11 17:41:35 +0530

    Add files via upload

---
+++ Mainexec.py
@@ -0,0 +1,57 @@
+from Processing import *
+
+import matplotlib.pyplot as plt
+from scipy import ndimage
+import numpy as np
+from statistics import variance as varn
+import time
+
+import os
+os.chdir('C:\\Users\Tamim\Stat Project Codes\Input Images')
+
+start=time.time()
+
+image=plt.imread('barcode3.jpg')
+
+binary_image= Saveto(image)
+
+def tobw(image):
+    return np.dot(image[...,:3], [1/2, 1/4, 1/4])
+
+img4=tobw(binary_image)
+(a,b)=img4.shape
+
+def countvar(image, angle, thres1, thres2):
+    rotimage= ndimage.rotate(image, angle)
+    (h,w)= rotimage.shape
+    ls=[]
+    for i in range(h):
+        count=0
+        for j in range(w-1):
+            if thres2 >= abs(rotimage[i, j+1]-rotimage[i,j]) >= thres1:
+                count +=1
+        ls.append(count)
+    var= varn(ls)
+    return var
+
+thres1,thres2= 60,130
+fnlist=[countvar(img4, angle, thres1,thres2) for angle in range(0,180)]
+B=min(fnlist)
+m= fnlist.index(B)
+
+def adjust(angle):
+    if angle >90:
+        return angle-90
+    else:
+        return angle +270
+
+fig, ax = plt.subplots(ncols=2, figsize=(20, 20))
+ax[0].imshow(image, cmap='gray')
+ax[1].imshow(ndimage.rotate(image, adjust(m)), cmap='gray')
+plt.show()
+
+end=time.time()
+    
+print('Skew about the positive X-axis is about :', 90-m, 'degrees')
+
+print(end-start)
---
+++ Processing.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def rgbtogray(image):
+    grayimg=np.dot(image[...,:3],[0.299, 0.587, 0.114])
+    return grayimg
+
+def threshold(image, thres=114):
+    img=rgbtogray(image)
+    (a,b)=img.shape
+    for i in range(a):
+        for j in range(b):
+            if img[i,j] >= thres:
+                img[i,j] =255
+            else:
+                img[i,j]=0
+    return img
+
+def Saveto(image):
+    img2=threshold(image)
+    plt.imshow(img2, cmap='gray')
+    plt.axis('off')
+    plt.savefig('rewritten.jpg')
+    
+    img3=plt.imread('rewritten.jpg')
+    return img3
+
+
+
+
+    
